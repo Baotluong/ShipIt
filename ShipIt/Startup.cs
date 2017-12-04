051a80da746e13a96333bb9a49bccf7d0dd87b3b
Bao Luong <baotluong@gmail.com>
2017-12-04 09:10:11 -0500

    Add project files.

---
+++ ShipIt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShipIt.Startup))]
+namespace ShipIt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
