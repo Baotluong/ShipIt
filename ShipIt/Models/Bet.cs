5910416697e286839c9ec0613c97ed62612363d4
Bao Luong <baotluong@gmail.com>
2017-12-04 09:28:41 -0500

    Init + created Bet Model and Linked with ApplicationUser

---
+++ ShipIt/Models/Bet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShipIt.Models
+{
+    public class Bet
+    {
+        public int Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public float BetFee { get; set; }
+
+        public int BetStatus { get; set; }
+
+        public virtual ICollection<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
--- ShipIt/Models/IdentityModels.cs
+++ ShipIt/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -16,6 +17,8 @@ namespace ShipIt.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public virtual ICollection<Bet> Bets { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
